82cb2ce42deab4b5dc4f3854231c21c7885500d1
Cristiano J <cjsantos86@gmail.com>
2016-04-12 16:19:14 -0300

    Initial version of Safety

---
+++ Safety0.0.1/app/src/main/java/com/example/cjsantos86/safety001/Main.java
@@ -0,0 +1,13 @@
+package com.example.cjsantos86.safety001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
