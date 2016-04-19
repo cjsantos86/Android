4f5d4f25b97293e2f207a1d8aec6e09d9c34766d
Cristiano J <cjsantos86@gmail.com>
2016-04-19 09:42:38 -0300

    List LenearLayout, with vehicle services

---
+++ ListViewExample/app/src/main/java/com/example/cjsantos86/listviewexample/myAdapter.java
@@ -0,0 +1,35 @@
+package com.example.cjsantos86.listviewexample;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.cjsantos86.listviewexample.R;
+
+class MyAdapter extends ArrayAdapter<String> {
+
+    public MyAdapter(Context context, String[] values) {
+        super(context, R.layout.row_layout2 , values);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater theInflator = LayoutInflater.from(getContext());
+        View theView = theInflator.inflate(R.layout.row_layout2, parent, false);
+
+        String autoServico = getItem(position);
+
+        TextView theTextView = (TextView) theView.findViewById(R.id.texView1);
+        theTextView.setText(autoServico);
+
+        ImageView theImageView = (ImageView) theView.findViewById(R.id.imageView1);
+        theImageView.setImageResource(R.drawable.tool);
+
+        return theView;
+
+    }
+}
